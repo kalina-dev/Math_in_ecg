31bfbba2cc53f17aa82252374d9c5a15cd830219
kalina-dev <110587154+kalina-dev@users.noreply.github.com>
2026-03-30 09:38:47 +0300

    improve code documentation ecg_generator.py

--- ecg_generator/notebooks/ecg_generator.py
+++ ecg_generator/notebooks/ecg_generator.py
@@ -11,14 +11,32 @@ def ecg_beat(t):
         data=json.load(file)
 
     #print(data)
+    # --- P WAVE ---
+    # Small positive bump representing atrial contraction
     cfg = data.get("P", {"a": 0.01, "b": 0.01, "c": 0.01})
     P = gaussian(t, cfg.get("a"), cfg.get("b"), cfg.get("c"))
+
+    # --- Q WAVE ---
+    # Small negative dip before the main spike
+    # Represents initial ventricular depolarization
     cfg = data.get("Q", {"a": 0.01, "b": 0.01, "c": 0.01})
     Q = gaussian(t, cfg.get("a"), cfg.get("b"), cfg.get("c"))
+
+    # --- R WAVE ---
+    # Large sharp peak (most prominent part of ECG)
+    # Represents full ventricular contraction
     cfg = data.get("R", {"a": 0.01, "b": 0.01, "c": 0.01})
     R = gaussian(t, cfg.get("a"), cfg.get("b"), cfg.get("c"))
+
+    # --- S WAVE ---
+    # Negative dip right after the R peak
+    # Completes ventricular depolarization
     cfg = data.get("S", {"a": 0.01, "b": 0.01, "c": 0.01})
     S = gaussian(t, cfg.get("a"), cfg.get("b"), cfg.get("c"))
+
+    # --- T WAVE ---
+    # Broader positive bump
+    # Represents ventricular recovery (repolarization)
     cfg = data.get("T", {"a": 0.01, "b": 0.01, "c": 0.01})
     T = gaussian(t, cfg.get("a"), cfg.get("b"), cfg.get("c"))
     return P + Q + R + S + T
@@ -53,6 +71,9 @@ def gaussian(t, mu, sigma, amplitude):
     """
     Gaussian wave function for ECG components
     """
+    """A Gaussian distribution, also known as a normal      distribution, is a continuous probability distribution characterized by its bell-shaped curve. It is defined by two parameters:
+    μ (mu): The mean or expected value of the distribution
+    σ (sigma): The standard deviation, which measures the spread of the distribution"""
     return amplitude * np.exp(-((t - mu)**2) / (2 * sigma**2))
 
 def generate_ecg(duration=5, heart_rate=70):
